cfdb903e0ae9a66c7409c10795eac9bb2cb901e9
bernackimark <bernackimark@gmail.com>
2024-05-15 23:41:04 -0400

    sundry code comments & clean up

--- streamlit_dg.py
+++ streamlit_dg.py
@@ -14,6 +14,7 @@ from events import EventResults
 
 MIN_POSSIBLE_DATE, MAX_POSSIBLE_DATE = date(1900, 1, 1), date(2099, 12, 31)
 TODAY = date.today()
+# TODO: utilnacki
 TIME_PERIODS = {
     'Last 30 Days': (TODAY - timedelta(days=30), TODAY),
     'Last Month': ((TODAY.replace(day=1) - timedelta(days=1)).replace(day=1), TODAY.replace(day=1) - timedelta(days=1)),
@@ -58,6 +59,7 @@ def populate_filters() -> dict:
     return {e: unique_sorted_values(e) for e in filter_elements}
 
 def populate_groupers() -> dict:
+    # TODO: can I separate key from display label, maybe using list.index() like in the each v cumulative radio?
     grouper_elements = ['player_w_flag', 'tourney_name', 'tourney_state', 'tourney_country',
                         'event_designation_map', 'event_year']
     return {'event_grouper': grouper_elements}
@@ -71,13 +73,13 @@ def filter_data(incoming_data: list[dict], filters: dict) -> list[dict]:
     # TODO: Move this to Utilnacki
     if not filters:
         return incoming_data
-    filtered = []
+    filtered = incoming_data
     for key, value in filters.items():
         if value and value != 'All':
             if key == 'time_period':
-                filtered = [entry for entry in incoming_data if value[0] <= entry['event_end_date'] <= value[1]]
+                filtered = [entry for entry in filtered if value[0] <= entry['event_end_date'] <= value[1]]
             else:
-                filtered = [entry for entry in incoming_data if entry[key] in value]
+                filtered = [entry for entry in filtered if entry[key] in value]
     return sorted(filtered, key=lambda x: x['event_end_date'], reverse=True)
 
 def group_data(data, grouper) -> list[dict]:
@@ -85,6 +87,7 @@ def group_data(data, grouper) -> list[dict]:
     The keys are blanks because streamlit datatables must have keys, else default column headers are shown. """
     # TODO: the empty string keys are going to be deprecated by Streamlit and are are stupid
     # TODO: can this be genericized and moved to utilnacki
+    # TODO: create a separate rank w ties function & move to utilnacki
     counter = Counter([row[grouper] for row in data if grouper])
     return sorted([{'': k, ' ': v} for k, v in counter.items()], key=lambda x: x[' '], reverse=True)
 
@@ -103,6 +106,7 @@ with st.sidebar:
     selected_divisions = st.sidebar.radio('Division', filter_map['player_division'], horizontal=True, index=2)
     filter_map['event_designation_map'].append('All')
     selected_designations = st.sidebar.radio('Designation', filter_map['event_designation_map'], horizontal=True, index=3)
+
     selected_tournament_names = st.sidebar.multiselect('Tournament', filter_map['tourney_name'])
 
     # Place state & country side-by-side sidebar into two columns
@@ -154,26 +158,27 @@ player_year_wins = wins_by_player_and_year(filtered_data, top_players)
 
 # DISPLAY THE DATA
 # Player Wins Line Chart
+# TODO: This should be sensitive to all parameter except time period, as it's too confusing ... how can i do this?
 with st.container():
     st.header('Top Players (DGPT Era)')
-    st.caption('Max five players, including ties.  Group by player to see the chart.')
+    st.caption('Max five players (plus ties).  Group by player to see the chart.')
     career_wins_options = ['Wins By Year', 'Cumulative Career Wins']
     each_or_cumulative = st.radio('', career_wins_options, horizontal=True)
     df = pd.DataFrame(player_year_wins[career_wins_options.index(each_or_cumulative)])
     chart = alt.Chart(df).mark_line().encode(x='year:O', y='wins:Q', color='player:N')
     st.altair_chart(chart, use_container_width=True)
 
+# TODO: This should show a rank column, and the existing left-most column is fubed
 # Leaderboard Table
 with st.container():
     st.header('Leaderboard (DGPT Era)')
     lb_col_config = {'player_w_flag': {'': st.column_config.Column('Winner', width='large')},
                      'event_year': {'': st.column_config.NumberColumn('Year', format='%d')},
-                     '_': {'': st.column_config.Column(selected_grouper.replace('_', ' ').title(), width='large')}}
+                     '_': {'': st.column_config.Column(selected_grouper.replace('_', ' ').title())}}
     st.dataframe(grouped_data, column_config=lb_col_config.get(selected_grouper) or lb_col_config['_'], hide_index=True)
 
 # Table
-with st.expander('All Results'):
-    st.header('All Results')
+with st.expander('Event Results'):
     column_order = ['event_year', 'player_w_flag', 'tourney_name', 'event_designation_map',
                     'player_division', 'tourney_state', 'tourney_country']
     column_config = {'event_year': st.column_config.NumberColumn('Year', format='%d'),
