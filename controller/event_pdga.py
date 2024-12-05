e426e21b161ed6f88b45122bb71ce607cc5b1ac4
bernackimark <bernackimark@gmail.com>
2024-12-04 21:06:44 -0500

    initial commit

---
+++ controller/event_pdga.py
@@ -0,0 +1,101 @@
+from datetime import date, datetime
+import json
+import requests
+
+from bs4 import BeautifulSoup
+from utilnacki.soup import list_of_dicts_from_soup_table
+
+
+EVENT_BASE_URL = 'https://www.pdga.com/tour/event/'
+
+
+class PDGAEvent:
+    PDGA_COMPLETED_EVENT_STATUS = 'Event complete; official ratings processed.'
+    EVENT_IDS_W_NO_PDGA_EVENT_ID = {6, 80, 159, 233}
+
+    def __init__(self, pdga_event_id: int):
+        self.pdga_event_id = pdga_event_id
+        self._scraped_data: dict = self._scrape_event_page()
+        self.data: dict = self._clean_division_result_data()
+
+    @property
+    def data_as_json(self):
+        return json.dumps(self.data)
+
+    @property
+    def end_date(self) -> date:
+        *_, end_date_str = self.data['dates'].split()
+        return datetime.strptime(end_date_str, "%d-%b-%Y").date()
+
+    @property
+    def status(self) -> str:
+        return self.data['status']
+
+    @property
+    def is_complete(self) -> bool:
+        return self.status == PDGAEvent.PDGA_COMPLETED_EVENT_STATUS
+
+    def _scrape_event_page(self) -> dict:
+        response = requests.get(f'{EVENT_BASE_URL}{self.pdga_event_id}')
+
+        if response.status_code != 200:
+            print(f"Failed to fetch from URL f'{EVENT_BASE_URL}{self.pdga_event_id}'. Status code: {response.status_code}")
+            exit()
+
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        divisions: list[str] = [div_tag['id'] for div_tag in soup.find_all(class_="division")]
+
+        # Find all tables on the page
+        tables = soup.find_all("table")
+
+        # the first table is the event status
+        event_status_html = tables[0]
+        event_status_data_row = event_status_html.find_all("tr")[1]
+
+        event_data = {'pdga_event_id': self.pdga_event_id,
+                      'status': event_status_data_row.find(class_="status").text,
+                      'player_cnt': event_status_data_row.find(class_="players").text,
+                      'purse': event_status_data_row.find(class_="purse").text,
+                      'dates': soup.find(class_="tournament-date").text,
+                      'name': soup.find("h1").text,
+                      'division_results': {}}
+
+        # Iterate over the division results table
+        for idx, table in enumerate(tables[1:]):  # the event status table is handled above
+            division = divisions[idx]
+            event_data['division_results'][division] = list_of_dicts_from_soup_table(table)
+
+        return event_data
+
+    def _clean_division_result_data(self) -> dict[str: list[dict]]:
+        """Cleans data from [{'Name': 'Drew Gibson', 'PDGA#': '48346', 'Par': 'E', '': ''} ...] to:
+        [{'Name': 'Drew Gibson', 'PDGA#': 48346, 'Par': 0} ...]"""
+        for div, table_data in self._scraped_data['division_results'].items():
+            if div not in ('MPO', 'FPO'):
+                continue
+            div_results_clean = []
+            for d_dirty in table_data:
+                d_clean = {}
+                for k, v in d_dirty.items():
+                    if k == '':  # ignoring round rating cols whose keys are ''
+                        continue
+                    if k == 'Par' and v == 'E':
+                        value = 0
+                    elif k in ('Place', 'PDGA#', 'Rating', 'Par', 'Rd1', 'Rd2', 'Rd3', 'Rd4', 'Finals', 'Total'):
+                        try:
+                            value = int(v)
+                        except ValueError:
+                            value = None  # handle DNFs
+                    elif k == 'Points':
+                        value = float(v) if v else 0
+                    elif k == 'Prize':
+                        value = float(v.replace("$", "").replace(",", "")) if v else 0
+                    else:
+                        value = v
+                    d_clean[k] = value
+                div_results_clean.append(d_clean)
+            self._scraped_data['division_results'][div] = div_results_clean
+
+        return self._scraped_data
