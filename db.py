f57320c47cb57e05f9d92accdac3b2eff0f481b1
bernackimark <bernackimark@gmail.com>
2024-12-18 18:47:21 -0500

    added function to yield a db cursor directly from psycopg2

--- db.py
+++ db.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
-from config import DB_CONN_STR
+from config import CONN_STR_UNPACKED, DB_CONN_STR
+import psycopg2
 
+# SQLAlchemy
 engine = create_engine(DB_CONN_STR)
-
-# Create a sessionmaker factory
 Session = sessionmaker(bind=engine)
 
 @contextmanager
 def get_db_session():
+    """Connects to and yields a database connection through SQLAlchemy's sessionmaker"""
     session = Session()
     try:
         yield session
@@ -20,3 +21,26 @@ def get_db_session():
         raise
     finally:
         session.close()
+
+
+@contextmanager
+def get_cursor_w_commit():
+    """Using raw psycopg2, connect to & yield a database connection.  Commits the transaction, if no error"""
+    conn = None
+    cursor = None
+    try:
+        conn = psycopg2.connect(CONN_STR_UNPACKED)
+        cursor = conn.cursor()
+        yield cursor
+        conn.commit()
+    except Exception as e:
+        # Roll back in case of exception
+        if conn:
+            conn.rollback()
+        raise
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
